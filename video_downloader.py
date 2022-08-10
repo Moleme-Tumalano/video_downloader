0ff323fe1fb23b3c2e7056471b5e7c01f83a4605
tmln46 <t.moleme46@gmail.com>
2022-08-10 13:00:17 +0200

    remod error of file

--- video_downloader.py
+++ video_downloader.py
@@ -1,35 +1,38 @@
 
 import re
 import sys
-from urllib import request
+import requests
 from bs4 import BeautifulSoup
 
+url = ''
 if len(sys.argv) > 1:
   url = sys.argv[1]
 else :
   sys.exit('Error: Please enter a URL')
 
-r = request.get(url)
+r = requests.get(url)
 
 print('Download about to start')
 
-soup = BeautifulSoup(r.content, features='lxml')
-
+soup = BeautifulSoup(r.content, 'html.parser')
+result = ''
 for val in soup.findAll("script"):
-  if(re.search('talkPage.init', str(val))) is not None:
+  if(re.search('props', str(val))) is not None:
     result = str(val)
 
-result_mp4 = re.search('?P<url>https?://[^\s]+)(mp4)', result).group('url')
 
-mp4_url =  result_mp4.split('"')[0]
+result_mp4 = re.search("(?P<url>https?://[^\s]+)(mp4)", result).group("url")
+
+mp4_url =  result_mp4 + "mp4"
 
 print("Downloading video from ........ " + mp4_url)
 
-file_name = mp4_url.split("/")[len(mp4_url.split("/"))-1].split('?')[0]
+file_name = mp4_url.split("/")[len(mp4_url.split("/"))-1][0:21] +".mp4"
+
 
 print("Storing video in ....... " + file_name)
 
-r = request.get(mp4_url)
+r = requests.get(mp4_url)
 
 with open (file_name, 'wb') as f:
   f.write(r.content)
